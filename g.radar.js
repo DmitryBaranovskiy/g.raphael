1068b54d59bc681e734c4262e94e257619a8c6c5
Silvan T. Golega <silvan@thinktec.(none)>
2009-12-14 13:28:58 +0100

    naming issue

--- g.radar.js
+++ g.radar.js
@@ -146,8 +146,8 @@ Raphael.fn.g.radar = function (cx, cy, r, values, opts) {
                     cover: cover,
                     cx: cx,
                     cy: cy,
-                    x: arm.x,
-                    y: arm.y,
+                    mx: arm.x,
+                    my: arm.y,
                     value: values[j],
                     max: max,
                     label: that.labels && that.labels[j]
