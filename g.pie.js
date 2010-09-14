f1e4f5db13a2fe8fe4bb7adb6956428f8854b56b
Sunny Ripert <sunny@sunfox.org>
2010-09-14 03:18:09 +0200

    Allow href on pie with only one element

--- g.pie.js
+++ g.pie.js
@@ -21,7 +21,7 @@ Raphael.fn.g.piechart = function (cx, cy, r, values, opts) {
     chart.covers = covers;
     if (len == 1) {
         series.push(this.circle(cx, cy, r).attr({fill: this.g.colors[0], stroke: opts.stroke || "#fff", "stroke-width": opts.strokewidth == null ? 1 : opts.strokewidth}));
-        covers.push(this.circle(cx, cy, r).attr(this.g.shim));
+        covers.push(this.circle(cx, cy, r).attr({href: opts.href ? opts.href[0] : null}).attr(this.g.shim));
         total = values[0];
         values[0] = {value: values[0], order: 0, valueOf: function () { return this.value; }};
         series[0].middle = {x: cx, y: cy};
