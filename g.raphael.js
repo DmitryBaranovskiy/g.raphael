bd4560ed8345a210fe450e44d4ed745ef1efb752
Richard Zurad <rzurad@gmail.com>
2011-10-22 21:25:04 +0000

    Removing 'darker', 'lighter', and 'original' functions into core repo as brightness plugin

--- g.raphael.js
+++ g.raphael.js
@@ -117,37 +117,4 @@
         return res;
     };
 */
-/*
- * TODO: these aren't used by any of the graphs
-    Raphael.el.lighter = function (times) {
-        times = times || 2;
-        var fs = [this.attrs.fill, this.attrs.stroke];
-        this.fs = this.fs || [fs[0], fs[1]];
-        fs[0] = Raphael.rgb2hsb(Raphael.getRGB(fs[0]).hex);
-        fs[1] = Raphael.rgb2hsb(Raphael.getRGB(fs[1]).hex);
-        fs[0].b = mmin(fs[0].b * times, 1);
-        fs[0].s = fs[0].s / times;
-        fs[1].b = mmin(fs[1].b * times, 1);
-        fs[1].s = fs[1].s / times;
-        this.attr({fill: "hsb(" + [fs[0].h, fs[0].s, fs[0].b] + ")", stroke: "hsb(" + [fs[1].h, fs[1].s, fs[1].b] + ")"});
-    };
-    Raphael.el.darker = function (times) {
-        times = times || 2;
-        var fs = [this.attrs.fill, this.attrs.stroke];
-        this.fs = this.fs || [fs[0], fs[1]];
-        fs[0] = Raphael.rgb2hsb(Raphael.getRGB(fs[0]).hex);
-        fs[1] = Raphael.rgb2hsb(Raphael.getRGB(fs[1]).hex);
-        fs[0].s = mmin(fs[0].s * times, 1);
-        fs[0].b = fs[0].b / times;
-        fs[1].s = mmin(fs[1].s * times, 1);
-        fs[1].b = fs[1].b / times;
-        this.attr({fill: "hsb(" + [fs[0].h, fs[0].s, fs[0].b] + ")", stroke: "hsb(" + [fs[1].h, fs[1].s, fs[1].b] + ")"});
-    };
-    Raphael.el.original = function () {
-        if (this.fs) {
-            this.attr({fill: this.fs[0], stroke: this.fs[1]});
-            delete this.fs;
-        }
-    };
-*/
 })();
